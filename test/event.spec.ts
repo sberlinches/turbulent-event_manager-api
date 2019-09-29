d29518b5761c605549729f3a9fbf14c51d10b0fa
Sergio Berlinches <sergioberlinches@gmail.com>
2019-09-29 21:21:15 +0200

    Added integration tests

---
+++ test/event.spec.ts
@@ -0,0 +1,13 @@
+import request from 'request';
+
+describe('/events', () => {
+
+  describe('POST', () => {
+    it('should be OK', (done) => {});
+    it('Should be wrong date', (done) => {});
+  });
+
+  describe('subscribeScheduledEvents', () => {
+    it('should be OK', (done) => {});
+  });
+});
