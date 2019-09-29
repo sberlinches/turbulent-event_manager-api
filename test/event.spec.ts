006aba3c1766ae28b3ed41b0dcdd4e6d3c016740
Sergio Berlinches <sergioberlinches@gmail.com>
2019-09-29 21:41:03 +0200

    Added POST endpoints

--- test/event.spec.ts
+++ test/event.spec.ts
@@ -1,10 +1,38 @@
 import request from 'request';
+// TODO: Implement mocks
 
 describe('/events', () => {
 
+  const uri = 'http://localhost:3000/events';
+  const options = {
+    json: true,
+    body: {},
+  };
+
   describe('POST', () => {
-    it('should be OK', (done) => {});
-    it('Should be wrong date', (done) => {});
+    it('should be OK', (done) => {
+
+      options.body = {
+        title: 'Hello world',
+        scheduledAt: new Date(),
+      };
+
+      request.post(uri, options, (error, response, body) => {
+        expect(response.statusCode).toEqual(200);
+        done();
+      });
+    });
+    it('Should be wrong parameters', (done) => {
+
+      options.body = {};
+
+      request.post(uri, options, (error, response, body) => {
+        expect(response.statusCode).toEqual(422);
+        done();
+      });
+    });
+    it('Should be wrong title', (done) => done());
+    it('Should be wrong date', (done) => done());
   });
 
   describe('subscribeScheduledEvents', () => {
